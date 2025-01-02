2dab0ebcea6310b49cb0757249492208cdf93626
Jose l. Azagra <azagramac@gmail.com>
2025-01-02 12:08:38 +0100

    first commit

---
+++ backup-config.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+unset file_backup
+
+host=$(hostname)
+folder_backup="/config"
+file_backup="config_backup_$(date +%d%m%Y_%H%M).tar.gz"
+
+server_user={USER_SERVER}
+server_backup={IP_SERVER}
+server_folder=/home/$server_user/ubiquiti/$host
+
+telegram_auth="/config/ssh-keys/telegram.env"
+token=$(cat $telegram_auth | grep "TOKEN" | awk -F "=" '{print $2}')
+chatid=$(cat $telegram_auth | grep "CHAT_ID" | awk -F "=" '{print $2}')
+api_sendMessage="https://api.telegram.org/bot$token/sendMessage?parse_mode=HTML"
+api_sendDocument="https://api.telegram.org/bot$token/sendDocument"
+
+if [ -d "$server_folder" ]; then
+    ssh $server_user@$server_backup ls -l $server_folder > /dev/null 2>&1
+else
+    ssh $server_user@$server_backup mkdir -p "$server_folder"
+fi
+
+cd /tmp
+tar -czvf $file_backup $folder_backup > /dev/null 2>&1
+check_md5_local=$(md5sum /tmp/$file_backup | awk '{print $1}')
+
+scp $file_backup $server_user@$server_backup:$server_folder/
+check_md5_server=$(ssh $server_user@$server_backup md5sum $server_folder/$file_backup | awk '{print $1}')
+verifyfile=$(ssh $server_user@$server_backup ls -l1t $server_folder | awk '{print $1, $9}' | head -2 | tail -1)
+
+if [ "$check_md5_local" == "$check_md5_server" ]; then
+    hashmd5="$check_md5_local"
+else
+    hashmd5="File $file_backup, hash MD5: $check_md5_server, md5 should be $check_md5_local"
+fi
+
+if [ "$check_md5_local" == "$check_md5_server" ]; then
+        curl -s -X POST $api_sendMessage -d chat_id=$chatid \
+        -d text="$(printf "✅ <b>Backup successfully created</b>\n\nHostname: $host\nMD5:  <code>$hashmd5 </code>\nFile:\n <code>$verifyfile</code>")" > /dev/null 2>&1
+        curl -s -X POST $api_sendDocument -F chat_id=$chatid -F document=@"$file_backup" > /dev/null 2>&1
+else
+        curl -s -X POST $api_sendMessage -d chat_id=$chatid \
+        -d text="$(printf "❌ <b>Backup failed, error MD5</b>\n\nHostname: $host\nError MD5:  <code>$hashmd5 </code>")" > /dev/null 2>&1
+fi
+
+rm -rf /tmp/$file_backup
